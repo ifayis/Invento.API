490f984a0650f1d1db8034f5691de0cc32030177
Fayis KV <mohammedfayis135@gmail.com>
2026-04-27 16:03:35 +0530

    adeed entity

---
+++ Invento.Domain/Entities/Base.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invento.Domain.Entities
+{
+    public abstract class Base
+    {
+        public int Id { get; set; }
+        public int TenantId { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ Invento.Domain/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invento.Domain.Entities
+{
+    public class Product : Base
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int quantity { get; set; }
+    }
+}
