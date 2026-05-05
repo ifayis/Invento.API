c26f057a66c837536958743fc044049665f04e1c
Fayis KV <mohammedfayis135@gmail.com>
2026-05-05 11:24:08 +0530

    added the product entity and command to add product

---
+++ Invento.Application/Features/Products/Commands/CreateProductCommand.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invento.Application.Features.Products.Commands
+{
+    public class CreateProductCommand : IRequest<Guid>
+    {
+        public string Name { get; set; } = string.Empty;
+        public string TagNumber { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        public decimal CostPrice { get; set; }
+        public decimal SellingPrice { get; set; }
+        public decimal TaxRate { get; set; }
+
+        public int StockQuanity { get; set; }
+        public string? ImageUrl { get; set; }
+
+    }
+}
---
+++ Invento.Domain/Entities/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invento.Domain.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public Guid TenantId { get; set; }
+        public Guid CategoryId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+        public string TagNumber { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        public decimal CostPrice { get; set; }
+        public decimal SellingPrice { get; set; }
+        public decimal TaxRate { get; set; }
+
+        public int StockQuanity { get; set; }
+        public string? ImageUrl { get; set; }
+        public DateTime CreatedAt { get; set; } 
+    }
+}
