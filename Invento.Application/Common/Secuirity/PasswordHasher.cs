5deae45cb0ac716504d99bb4337947c3e9f757d1
Fayis KV <mohammedfayis135@gmail.com>
2026-05-05 14:18:05 +0530

    create hash password component

---
+++ Invento.Application/Common/Secuirity/PasswordHasher.cs
@@ -0,0 +1,22 @@
+﻿using BCrypt.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invento.Application.Common.Secuirity
+{
+    public static class PasswordHasher
+    {
+        public static string Hash(string Password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(Password);
+        }
+
+        public static bool Verify(string password, string hash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hash);
+        }
+    }
+}
