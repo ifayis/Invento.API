98a139f306502bc27e1119490b6394192b1669cc
Fayis KV <mohammedfayis135@gmail.com>
2026-05-05 16:02:10 +0530

    added the auth controller

---
+++ Invento.API/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using Invento.Application.Features.Auth.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Invento.API.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AuthController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("signup")]
+        public async Task<IActionResult> Register(RegisterCommand command)
+        {
+            var token = await _mediator.Send(command);
+            return Ok(token);
+        }
+
+        [HttpPost("signin")]
+        public async Task<IActionResult> Login(LoginCommand command)
+        {
+            var token = await _mediator.Send(command);
+            return Ok(token);
+        }
+    }
+}
