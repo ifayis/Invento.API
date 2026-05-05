8ef97898b0609e3678b383594a054cb1152c0a2b
Fayis KV <mohammedfayis135@gmail.com>
2026-05-05 12:44:01 +0530

    added the controller for adding new products

---
+++ Invento.API/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Invento.Application.Features.Products.Commands;
+
+namespace Invento.API.Controllers
+{
+    [ApiController]
+    [Route("api/products")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("add")]
+        public async Task<IActionResult> Create(CreateProductCommand command)
+        {
+            var id = await _mediator.Send(command);
+            return Ok(id);
+        }
+    }
+}
