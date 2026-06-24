c3c66c90c0de603fac7b777073ba7ea04c4447f3
Fayis KV <mohammedfayis135@gmail.com>
2026-06-24 23:30:34 +0530

    done latest migration

--- Invento.Persistence/Migrations/20260624175453_latestupdate6.Designer.cs
+++ Invento.Persistence/Migrations/20260624175453_latestupdate6.Designer.cs
@@ -4,6 +4,7 @@ using Invento.Persistence.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Invento.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260624175453_latestupdate6")]
+    partial class latestupdate6
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Invento.Persistence/Migrations/20260624175453_latestupdate6.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Invento.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class latestupdate6 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PurchaseItems_Products_ProductId1",
+                table: "PurchaseItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_SaleItems_Products_ProductId1",
+                table: "SaleItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SaleItems_ProductId1",
+                table: "SaleItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PurchaseItems_ProductId1",
+                table: "PurchaseItems");
+
+            migrationBuilder.DropColumn(
+                name: "ProductId1",
+                table: "SaleItems");
+
+            migrationBuilder.DropColumn(
+                name: "ProductId1",
+                table: "PurchaseItems");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Role",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Customers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Customers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeletedBy",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Customers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedBy",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedBy",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "Customers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Role",
+                table: "Users",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ProductId1",
+                table: "SaleItems",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ProductId1",
+                table: "PurchaseItems",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SaleItems_ProductId1",
+                table: "SaleItems",
+                column: "ProductId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PurchaseItems_ProductId1",
+                table: "PurchaseItems",
+                column: "ProductId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PurchaseItems_Products_ProductId1",
+                table: "PurchaseItems",
+                column: "ProductId1",
+                principalTable: "Products",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SaleItems_Products_ProductId1",
+                table: "SaleItems",
+                column: "ProductId1",
+                principalTable: "Products",
+                principalColumn: "Id");
+        }
+    }
+}
