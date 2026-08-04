999db3ed8b7d626e456d81361ac5f420cf5ad09d
Fayis KV <mohammedfayis135@gmail.com>
2026-08-05 00:40:25 +0530

    added json ignore from update endpoint to avoid conflict

--- Invento.Application/Features/Suppliers/Commands/UpdateSupplierCommand.cs
+++ Invento.Application/Features/Suppliers/Commands/UpdateSupplierCommand.cs
@@ -1,12 +1,14 @@
 ﻿using Invento.Application.Abstractions;
 using Invento.Application.Common;
 using Invento.Application.Features.Suppliers.DTOs;
+using System.Text.Json.Serialization;
 
 namespace Invento.Application.Features.Suppliers.Commands
 {
     public class UpdateSupplierCommand
         : ICommand<ApiResponse<SupplierDto>>
     {
+        [JsonIgnore]
         public Guid Id { get; set; }
 
         public string Name { get; set; }
